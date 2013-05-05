cc8defe2fed98fefeb8e5f6dc3c288f409a83322
mokko <mauricemengel@gmail.com>
2013-05-05 09:08:01 -0400

    use lib instead unshift INC; use filepath instead of module name; add evals to prevent from dieing when encounting problems.

--- lib/Pod/Weaver/Section/Consumes.pm
+++ lib/Pod/Weaver/Section/Consumes.pm
@@ -2,83 +2,87 @@ package Pod::Weaver::Section::Consumes;
 
 use strict;
 use warnings;
-
+use Module::Load;
+use lib './lib';    #instead messing with INC
 
 # ABSTRACT: Add a list of roles to your POD.
-
 use Moose;
-use Module::Load;
 with 'Pod::Weaver::Role::Section';
 
 use aliased 'Pod::Elemental::Element::Nested';
 use aliased 'Pod::Elemental::Element::Pod5::Command';
 
-sub weave_section { 
+sub weave_section {
     my ( $self, $doc, $input ) = @_;
 
-    my $file = $input->{filename};
-    return unless $file =~ m{^lib/};
+    my $filename = $input->{filename};    #full rel path
+    return unless $filename =~ m{^lib/};
 
-    my $module = $file;
-    $module =~ s{^lib/}{};    # assume modules live under lib
+    # works only if one package pro file
+    my $inc_filename = $filename;         #as in %INC's keys
+    $inc_filename =~ s{^lib/}{};          # assume modules live under lib
+    my $module = $inc_filename;
     $module =~ s{/}{::}g;
-    $module =~ s/\.pm//;
+    $module =~ s{\.\w+$}{};
 
-    unshift @INC, './lib';    # assume we want modules from the CWD
+    eval { load $inc_filename };
+    print "$@" if $@;
+    #print map {"$_\n"} sort keys %INC;
 
-    load $module;
-
-    return unless $module->can( 'meta' );
-
+    return unless $module->can('meta');
     my @roles = sort
-        grep { $_ ne $module }
-        map  { $_->name      }
-        $self->_get_roles( $module );
-
+      grep { $_ ne $module }
+      map  { $_->name } $self->_get_roles($module);
     return unless @roles;
 
     my @pod = (
-        Command->new( { 
-            command   => 'over',
-            content   => 4
-        } ),
+        Command->new(
+            {
+                command => 'over',
+                content => 4
+            }
+        ),
 
-        ( map { 
-            Command->new( {
-                command    => 'item',
-                content    => "* L<$_>",
-            } ),
-        } @roles ),
+        (
+            map {
+                Command->new(
+                    {
+                        command => 'item',
+                        content => "* L<$_>",
+                    }
+                  ),
+            } @roles
+        ),
 
-        Command->new( { 
-            command   => 'back',
-            content   => ''
-        } )
-    );        
+        Command->new(
+            {
+                command => 'back',
+                content => ''
+            }
+        )
+    );
 
     push @{ $doc->children },
-        Nested->new( { 
-            type      => 'command',
-            command   => 'head1',
-            content   => 'CONSUMES',
-            children  => \@pod
-        } );
-
-    shift @INC;
+      Nested->new(
+        {
+            type     => 'command',
+            command  => 'head1',
+            content  => 'CONSUMES',
+            children => \@pod
+        }
+      );
 
 }
 
-sub _get_roles { 
+sub _get_roles {
     my ( $self, $module ) = @_;
 
-    my @roles = $module->meta->calculate_all_roles;
-
+    my @roles = eval { $module->meta->calculate_all_roles };
+    print "Possibly harmless: $@" if $@;
     return @roles;
 }
 
-
 1;
-
 
 =pod
 
